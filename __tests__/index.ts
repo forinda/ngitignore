8ae269aaf3e9f8c9f311714859f3eee9a288c786
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-27 22:59:03 +0300

    Update tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,9 +1,5 @@
-import { help } from './../src/commads/help';
 import { zero } from './../src/commads/zero';
 
 test('Help status', () => {
 	expect(zero()).toBe(0);
-});
-test('Help status', () => {
-	expect(help()).toBe(0);
 });
